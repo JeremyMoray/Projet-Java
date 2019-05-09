7f2bc6ea931f6153398c26ad5615979d617f185c
JeremyMoray <etu37064@henallux.be>
2019-05-09 12:41:17 +0200

    Commit principal

---
+++ src/businessPackage/Manager.java
@@ -0,0 +1,91 @@
+package businessPackage;
+
+import java.util.*;
+import exceptionPackage.*;
+import modelPackage.*;
+import dataAccessPackage.*;
+
+public class Manager {
+
+    private DataAccess dao;
+
+    public Manager() {
+        setDao(new DBAccess()) ;
+    }
+
+    public void closeConnection() throws AccesDBException{
+        dao.closeConnection();
+    }
+
+    public void addSoignant (Soignant soignant) throws AccesDBException, ChampsExistantException {
+        dao.addSoignant(soignant);
+    }
+
+    public Soignant getSoignant(String numNat, String motDePasse) throws AccesDBException, ConnexionException{
+        return dao.getSoignant(numNat, motDePasse);
+    }
+
+    public Soignant getSoignant(Integer soignant_id) throws AccesDBException {
+        return dao.getSoignant(soignant_id);
+    }
+
+    public void updateSoignant(Soignant soignantModifié, Integer soignant_id) throws AccesDBException, ChampsExistantException {
+        dao.updateSoignant(soignantModifié, soignant_id);
+    }
+
+    public void deleteSoignant(Integer soignant_id) throws AccesDBException {
+        dao.deleteSoignant(soignant_id);
+    }
+
+    public void addMutualite(Mutualite mutualite) throws AccesDBException {
+        dao.addMutualite(mutualite);
+    }
+
+    public ArrayList<Mutualite> getAllMutualites( ) throws AccesDBException, ChampsVideException, CaracteresLimiteException {
+        return dao.getAllMutualites( );
+    }
+
+    public void deleteMutualite(Integer mutualite_id) throws AccesDBException {
+        dao.deleteMutualite(mutualite_id);
+    }
+
+    public void updateMutualite(Mutualite mutualite) throws AccesDBException{
+        dao.updateMutualite(mutualite);
+    }
+
+    public void addAllergie(Allergie allergie) throws AccesDBException {
+        dao.addAllergie(allergie);
+    }
+
+    public ArrayList<Allergie> getAllAllergies() throws AccesDBException, ChampsVideException, CaracteresLimiteException{
+        return dao.getAllAllergies( );
+    }
+
+    public void deleteAllergie(Integer allergie_id) throws AccesDBException {
+        dao.deleteAllergie(allergie_id);
+    }
+
+    public void updateAllergie(Allergie allergie) throws AccesDBException{
+        dao.updateAllergie(allergie);
+    }
+
+    public void addMedicament(Medicament medicament) throws AccesDBException {
+        dao.addMedicament(medicament);
+    }
+
+    public ArrayList<Medicament> getAllMedicaments() throws AccesDBException, ChampsVideException, CaracteresLimiteException, CodeInvalideException, FormatNombreException {
+        return dao.getAllMedicaments();
+    }
+
+    public void deleteMedicament(Integer medicament_id) throws AccesDBException {
+        dao.deleteMedicament(medicament_id);
+    }
+
+    public void updateMedicament(Medicament medicament) throws AccesDBException{
+        dao.updateMedicament(medicament);
+    }
+
+    public void setDao(DBAccess dao) {
+        this.dao = dao;
+    }
+}
