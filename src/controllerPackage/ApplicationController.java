283a58caa3cc95ff3f7a3f7e5862e322c7774325
JeremyMoray <etu37064@henallux.be>
2019-05-09 12:44:49 +0200

    Création ApplicationController

---
+++ src/controllerPackage/ApplicationController.java
@@ -0,0 +1,91 @@
+package controllerPackage;
+
+import java.util.*;
+import businessPackage.*;
+import exceptionPackage.*;
+import modelPackage.*;
+
+public class ApplicationController {
+
+    private Manager manager;
+
+    public ApplicationController(){
+        setManager(new Manager());
+    }
+
+    public void closeConnection() throws AccesDBException{
+        manager.closeConnection();
+    }
+
+    public void addSoignant(Soignant soignant) throws AccesDBException, ChampsExistantException {
+        manager.addSoignant(soignant);
+    }
+
+    public Soignant getSoignant(String numNat, String motDePasse) throws AccesDBException, ConnexionException{
+        return manager.getSoignant(numNat, motDePasse);
+    }
+
+    public Soignant getSoignant(Integer soignant_id) throws AccesDBException {
+        return manager.getSoignant(soignant_id);
+    }
+
+    public void updateSoignant(Soignant soignantModifié, Integer soignant_id) throws AccesDBException, ChampsExistantException {
+        manager.updateSoignant(soignantModifié, soignant_id);
+    }
+
+    public void deleteSoignant(Integer soignant_id) throws AccesDBException {
+        manager.deleteSoignant(soignant_id);
+    }
+
+    public void addMutualite(Mutualite mutualite) throws AccesDBException {
+        manager.addMutualite(mutualite);
+    }
+
+    public ArrayList<Mutualite> getAllMutualites() throws AccesDBException, ChampsVideException, CaracteresLimiteException {
+        return manager.getAllMutualites();
+    }
+
+    public void deleteMutualite(Integer mutualite_id) throws AccesDBException {
+        manager.deleteMutualite(mutualite_id);
+    }
+
+    public void updateMutualite(Mutualite mutualite) throws AccesDBException{
+        manager.updateMutualite(mutualite);
+    }
+
+    public void addAllergie(Allergie allergie) throws AccesDBException {
+        manager.addAllergie(allergie);
+    }
+
+    public ArrayList<Allergie> getAllAllergies() throws AccesDBException, ChampsVideException, CaracteresLimiteException {
+        return manager.getAllAllergies();
+    }
+
+    public void deleteAllergie(Integer allergie_id) throws AccesDBException {
+        manager.deleteAllergie(allergie_id);
+    }
+
+    public void updateAllergie(Allergie allergie) throws AccesDBException{
+        manager.updateAllergie(allergie);
+    }
+
+    public void addMedicament(Medicament medicament) throws AccesDBException {
+        manager.addMedicament(medicament);
+    }
+
+    public ArrayList<Medicament> getAllMedicaments() throws AccesDBException, ChampsVideException, CaracteresLimiteException, CodeInvalideException, FormatNombreException {
+        return manager.getAllMedicaments();
+    }
+
+    public void deleteMedicament(Integer medicament_id) throws AccesDBException {
+        manager.deleteMedicament(medicament_id);
+    }
+
+    public void updateMedicament(Medicament medicament) throws AccesDBException{
+        manager.updateMedicament(medicament);
+    }
+
+    public void setManager(Manager manager){
+        this.manager = manager;
+    }
+}
