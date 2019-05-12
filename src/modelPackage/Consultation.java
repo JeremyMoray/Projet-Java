9701689056b72531c7f24c103620644f0aad165a
JeremyMoray <etu37064@henallux.be>
2019-05-12 14:29:10 +0200

    Rajout des 2 classes pour la fenetre et le modèle consultation

---
+++ src/modelPackage/Consultation.java
@@ -0,0 +1,60 @@
+package modelPackage;
+
+import exceptionPackage.CaracteresLimiteException;
+import exceptionPackage.ChampsVideException;
+
+import java.util.GregorianCalendar;
+
+public class Consultation {
+    private Integer soignant_id;
+    private Integer patient_id;
+    private String observations;
+    private GregorianCalendar dateConsultation;
+
+    public Consultation(Integer soignant_id, Integer patient_id, String observations, GregorianCalendar dateConsultation) throws CaracteresLimiteException, ChampsVideException {
+        setSoignant_id(soignant_id);
+        setPatient_id(patient_id);
+        setObservations(observations);
+        setDateConsultation(dateConsultation);
+    }
+
+    public Integer getSoignant_id() {
+        return soignant_id;
+    }
+
+    public Integer getPatient_id() {
+        return patient_id;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public GregorianCalendar getDateConsultation() {
+        return dateConsultation;
+    }
+
+    public void setSoignant_id(Integer soignant_id) {
+        this.soignant_id = soignant_id;
+    }
+
+    public void setPatient_id(Integer patient_id) {
+        this.patient_id = patient_id;
+    }
+
+    public void setObservations(String observations) throws CaracteresLimiteException{
+        if(observations.isEmpty()){
+            if(observations.length() > 250){
+                throw new CaracteresLimiteException("Observations");
+            }
+        }
+        this.observations = observations;
+    }
+
+    public void setDateConsultation(GregorianCalendar dateNaissance) throws ChampsVideException {
+        if(dateNaissance == null){
+            throw new ChampsVideException("Date de consultation");
+        }
+        this.dateConsultation = dateNaissance;
+    }
+}
---
+++ src/viewPackage/FenetreAjouterConsultation.java
@@ -0,0 +1,197 @@
+package viewPackage;
+
+import controllerPackage.ApplicationController;
+import exceptionPackage.*;
+import modelPackage.Consultation;
+import modelPackage.Soignant;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class FenetreAjouterConsultation extends JFrame {
+
+    private Soignant utilisateur;
+    private Integer patient_id;
+    private ApplicationController controller;
+    private GridBagConstraints gbc = new GridBagConstraints();
+    private JSpinner jourSpinner, moisSpinner, anneeSpinner, heureSpinner, minuteSpinner;
+    private JTextField observationsField;
+    private JLabel observationLabel;
+    private JButton enregistrerBouton, annulerBouton;
+    private GregorianCalendar dateConsultation;
+    private Consultation consultation;
+
+    public FenetreAjouterConsultation(Soignant utilisateur, Integer patient_id){
+        super("Ajouter une consultation");
+        setBounds((int)(FenetreMenu.getWindowWidth() * 0.30), (int)(FenetreMenu.getWindowHeight() * 0.30),
+                (int)(FenetreMenu.getWindowWidth() * 0.40), (int)(FenetreMenu.getWindowHeight() * 0.40));
+        setController(new ApplicationController());
+        this.utilisateur = utilisateur;
+        this.patient_id = patient_id;
+        this.setLayout(new GridBagLayout());
+        dateConsultation = new GregorianCalendar();
+
+        int anneeActuelle = GregorianCalendar.getInstance().get(GregorianCalendar.YEAR);
+
+        SpinnerModel modelJour = new SpinnerNumberModel(1, 1, 31, 1);
+        SpinnerModel modelMois = new SpinnerNumberModel(1, 1, 12, 1);
+        SpinnerModel modelAnnee = new SpinnerNumberModel(anneeActuelle, anneeActuelle, anneeActuelle + 10, 1);
+        SpinnerModel modelHeure = new SpinnerNumberModel(12, 0, 23, 1);
+        SpinnerModel modelMinute = new SpinnerNumberModel(0, 0, 59, 5);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.ipadx = 15;
+        gbc.ipady = 10;
+        gbc.insets = new Insets(10,10,10,10);
+        jourSpinner = new JSpinner(modelJour);
+        this.add(jourSpinner, gbc);
+
+        gbc.gridx = 1;
+        this.add(new JLabel("/"));
+
+        gbc.gridx = 2;
+        moisSpinner = new JSpinner(modelMois);
+        this.add(moisSpinner, gbc);
+
+        gbc.gridx = 3;
+        this.add(new JLabel("/"));
+
+        gbc.gridx = 4;
+        anneeSpinner = new JSpinner(modelAnnee);
+        this.add(anneeSpinner, gbc);
+
+        gbc.gridx = 5;
+        gbc.insets = new Insets(10,30,10,10);
+        heureSpinner = new JSpinner(modelHeure);
+        this.add(heureSpinner, gbc);
+
+        gbc.gridx = 6;
+        gbc.insets = new Insets(10,10,10,10);
+        this.add(new JLabel("H -"));
+
+        gbc.gridx = 7;
+        minuteSpinner = new JSpinner(modelMinute);
+        this.add(minuteSpinner, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.ipadx = 0;
+        gbc.insets = new Insets(10,0,10,0);
+        observationLabel = new JLabel("Observations :");
+        this.add(observationLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.ipadx = 300;
+        gbc.gridwidth = 7;
+        observationsField = new JTextField();
+        this.add(observationsField, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.ipadx = 50;
+        gbc.gridwidth = 3;
+        enregistrerBouton = new JButton("Enregistrer");
+        ButtonListener enregistrerButtonListener = new ButtonListener();
+        enregistrerBouton.addActionListener(enregistrerButtonListener);
+        this.add(enregistrerBouton, gbc);
+
+        gbc.gridx = 5;
+        gbc.gridy = 2;
+        annulerBouton = new JButton("Annuler");
+        ButtonListener annulerButtonListener = new ButtonListener();
+        annulerBouton.addActionListener(annulerButtonListener);
+        this.add(annulerBouton, gbc);
+
+        setVisible(true);
+    }
+
+    private class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+
+            if(event.getSource() == enregistrerBouton){
+                try{
+                    try{
+                        Integer jour = Integer.parseInt(jourSpinner.getValue().toString());
+                        Integer mois = Integer.parseInt(moisSpinner.getValue().toString())-1;
+                        Integer annee = Integer.parseInt(anneeSpinner.getValue().toString());
+                        Integer heure = Integer.parseInt(heureSpinner.getValue().toString());
+                        Integer minute = Integer.parseInt(minuteSpinner.getValue().toString());
+
+                        if(jour == null){
+                            throw new ChampsVideException("Date de consultation (Jour)");
+                        }
+                        if(mois == null){
+                            throw new ChampsVideException("Date de consultation (Mois)");
+                        }
+                        if(annee == null){
+                            throw new ChampsVideException("Date de consultation (Annee)");
+                        }
+                        if(heure == null){
+                            throw new ChampsVideException("Date de consultation (Heure)");
+                        }
+                        if(minute == null){
+                            throw new ChampsVideException("Date de consultation (Annee)");
+                        }
+
+                        Calendar dateActuelle = Calendar.getInstance();
+
+                        if(jour < 0 || jour > 31){
+                            throw new FormatDateException();
+                        }
+                        if(mois < 0 || mois > 12){
+                            throw new FormatDateException();
+                        }
+                        if(annee < dateActuelle.get(Calendar.YEAR) || annee > dateActuelle.get(Calendar.YEAR) + 10){
+                            throw new FormatDateException();
+                        }
+                        dateConsultation.set(GregorianCalendar.DAY_OF_MONTH, jour-1);
+                        dateConsultation.set(GregorianCalendar.MONTH, mois);
+                        dateConsultation.set(GregorianCalendar.YEAR, annee);
+                        dateConsultation.set(GregorianCalendar.HOUR, heure);
+                        dateConsultation.set(GregorianCalendar.MINUTE, minute);
+
+                        if(dateConsultation.get(GregorianCalendar.MONTH) != mois){
+                            throw new FormatDateException();
+                        }
+
+                        if(dateConsultation.compareTo(dateActuelle) < 0){
+                            throw new FormatDateException();
+                        }
+                    }
+                    catch(NumberFormatException exception){
+                        throw new FormatDateException();
+                    }
+
+                    consultation = new Consultation(utilisateur.getId(), patient_id, observationsField.getText(), dateConsultation);
+                    controller.addConsultation(consultation);
+                    JOptionPane.showMessageDialog(null, "La consultation a été ajoutée", "Erreur", JOptionPane.INFORMATION_MESSAGE);
+                }
+                catch (AccesDBException exception){
+                    JOptionPane.showMessageDialog(null, exception.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+                catch (ChampsVideException exception){
+                    JOptionPane.showMessageDialog(null, exception.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+                catch (CaracteresLimiteException exception){
+                    JOptionPane.showMessageDialog(null, exception.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+                catch (FormatDateException exception){
+                    JOptionPane.showMessageDialog(null, exception.getMessage(), "Erreur", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+
+            dispose();
+        }
+    }
+
+    public void setController(ApplicationController controller){
+        this.controller = controller;
+    }
+}
+
