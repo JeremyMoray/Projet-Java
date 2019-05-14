c17c8bf7eb5d7b4aadf61c85d1c5337ed08a3c54
JeremyMoray <etu37064@henallux.be>
2019-05-14 23:04:57 +0200

    Re-correction JUnit

--- src/businessPackage/PrimeTest.java
+++ src/businessPackage/PrimeTest.java
@@ -1,16 +1,8 @@
 package businessPackage;
 
-import org.jboss.arquillian.container.test.api.Deployment;
-import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.asset.EmptyAsset;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 public class PrimeTest {
 
@@ -22,7 +14,7 @@ public class PrimeTest {
     }
 
     @Test
-    public void addPrime() {
+    public void addPrime() throws Exception{
         Assert.assertEquals (1000., manager.addPrime(800.,200.), 0.001);
     }
 }
